eccacadd68675badcc43a83df7ee4b26edca0138
Javohir3737 <javamirzadavlatov000@gmail.com>
2024-03-01 10:35:57 +0500

    first commit

---
+++ models/user.model.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  telnomer: {
+    type: String,
+    required: true,
+  },
+  coin: {
+    type: String,
+    required: true,
+    
+  },
+  vaqti: {
+    type: String,
+    required: true,
+  },
+ 
+});
+
+module.exports = model("User", userSchema);
